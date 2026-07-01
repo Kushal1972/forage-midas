f7eb7f73d7249b668e67ffe14756cafd3b9b97e6
Kushal <kushalkuhad@gmail.com>
2026-07-02 03:26:10 +0530

    Complete JPMorgan Chase Midas Core Virtual Experience

--- src/main/java/com/jpmc/midascore/component/DatabaseConduit.java
+++ src/main/java/com/jpmc/midascore/component/DatabaseConduit.java
@@ -3,9 +3,9 @@ package com.jpmc.midascore.component;
 import com.jpmc.midascore.entity.UserRecord;
 import com.jpmc.midascore.repository.UserRepository;
 import org.springframework.stereotype.Component;
-
 @Component
 public class DatabaseConduit {
+
     private final UserRepository userRepository;
 
     public DatabaseConduit(UserRepository userRepository) {
